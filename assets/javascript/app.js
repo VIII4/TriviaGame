65fe662c3cc6c26f7e8678efea0d526e3e79d400
Lay84 <Iyan.laylor@gmail.com>
2020-02-21 10:35:13 -0800

    added 30 questions with associated images for scifi theme

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,6 +9,7 @@ var answerBElement;
 var answerCElement;
 var answerDElement;
 var gameStartButton;
+var gameOverButton;
 
 //Round Result Panel
 var resultsPanel;
@@ -46,8 +47,9 @@ var roundTimeInteravl;
 var queueTime = 0;
 var queueTimeInterval;
 
+//All Questions Container
 var allQuestions = [];
-
+//Random Questions Selected for round
 var selectedQuestions = [];
 
 var currentQuestions;
@@ -114,22 +116,385 @@ var templateQuestion = new QuestionObject(
   " Option 4",
   "assets/images/test.jpg"
 );
+var questionOne = new QuestionObject(
+  "Which of these science fiction movies stars David Bowie as Jareth, the Goblin King?",
 
-var templateQuestion2 = new QuestionObject(
-  " Question2",
-  " Correct Answer2",
-  " Option 22",
-  " Otions 32",
-  " Option 42",
-  "assets/images/hello.png"
+  "Labyrinth",
+  "The Dark Crystal",
+  "Legend",
+  "Time Piece",
+
+  "assets/images/labyrinth.jpg"
+);
+var questionTwo = new QuestionObject(
+  "Directed by Andrew Niccol, which movie features a society where eugenics is common and DNA plays a major role in determining one's social class?",
+
+  "Gattacaa",
+  "THX 1138",
+  "Daybreakers",
+  "In Time",
+
+  "assets/images/gattaca.jpeg"
+);
+var questionThree = new QuestionObject(
+  "Which of the following films stars Mel Gibson as Max Rockatansky?",
+
+  "The Road Warrior",
+  "Waterworld",
+  "After Earth",
+  "Doomsday",
+
+  "assets/images/theroadwarrior.jpg"
+);
+var questionFour = new QuestionObject(
+  "Released in 2004, which movie depicts the story of two young engineers who accidently invent a time machine?",
+
+  "Primer",
+  "Timecrimes",
+  "Upstream Color",
+  "Looper",
+
+  "assets/images/primer.jpg"
+);
+var questionFive = new QuestionObject(
+  "In the movie, 'The Matrix,' what was the name of the computer hacker who discovered that all life on Earth was actually a simulated reality?",
+
+  "Neo",
+  "Theo",
+  "Sam",
+  "Leo",
+
+  "assets/images/neomatrix.jpg"
+);
+var questionSix = new QuestionObject(
+  "Directed by Fritz Lang and released in 1927, what was the first full-length science fiction movie ever made?",
+
+  "Metropolis",
+  "Battleship Potemkin",
+  "The Blue Angel",
+  "The Last Laugh",
+
+  "assets/images/metropolis.jpg"
+);
+var questionSeven = new QuestionObject(
+  "Which of the following science fiction films is based on a 1961 novel by Polish writer Stanislaw Lem?",
+
+  "Solaris",
+  "First Spaceship on Venus",
+  "The Invasion",
+  "Planet of the Apes",
+
+  "assets/images/solaris.jpg"
+);
+var questionEight = new QuestionObject(
+  "Living in harmony with nature, which film features the Na'vi, 10-foot tall blueskinned humanoids?",
+
+  "Avatar",
+  "Star Trek",
+  "Tron",
+  "Pacific Rim",
+
+  "assets/images/avatar.jpg"
+);
+var questionNine = new QuestionObject(
+  "In which movie do we see Marty McFly wayfaring back through time in a DeLorean to interfere in his parents' love story?",
+
+  "Back to the Future",
+  "Time After Time",
+  "The Time Machine",
+  "The Goonies",
+
+  "assets/images/backtothefuture.jpg"
+);
+var questionTen = new QuestionObject(
+  "Christened the greatest science-fiction film ever made according to Ray Bradbury, in what year was 'Close Encounters of the Third Kind' released?",
+
+  "1977",
+  "1997",
+  "1967",
+  "1987",
+
+  "assets/images/closeencounters.jpg"
+);
+var questionEleven = new QuestionObject(
+  "Which science fiction film tells the story of a health-food storeowner who is cryogenically frozen and wakes up in 2173 during a rebellion?",
+
+  "Sleeper",
+  "Scoop",
+  "Reds",
+  "Idiocracy",
+
+  "assets/images/sleeper.jpg"
+);
+var questionTwelve = new QuestionObject(
+  "Which movie features an alien humanoid named Klaatu that visits earth with a message for humankind?",
+
+  "The Day the Earth Stood Still",
+  "War of the Worlds",
+  "The Day the Earth Stopped",
+  "The Day After Tomorrow",
+
+  "assets/images/daytheearthstoodstill.jpg"
+);
+var questionThirteen = new QuestionObject(
+  "In the science fiction thriller 'Terminator 3,' who played the role of T-X, the first onscreen female Terminator?",
+
+  "Kristanna Loken",
+  "Katherine Moennig",
+  "Claire Danes",
+  "Michelle Rodriguez",
+  /////////Need TX Image/////////////
+  "assets/images/test.jpg"
+);
+var questionFourteen = new QuestionObject(
+  "Released in 2005, which film is a continuation of Joss Whedon's short-lived 2002 television series 'Firefly'?",
+
+  "Serenity",
+  "The Chronicles of Riddick",
+  "Ender's Game",
+  "Slither",
+
+  "assets/images/sereniity.jpg"
+);
+var questionFifteen = new QuestionObject(
+  "What character does Felicity Jones play in the 2016 Walt Disney film 'Star Wars Rogue One'?",
+
+  "Jyn Erso",
+  "Rey",
+  "Padme Amidala",
+  "Leia Organa",
+
+  "assets/images/rougueone.jpg"
+);
+var questionSixteen = new QuestionObject(
+  "'Bruce Willis' and 'Brad Pitt' star in what 1995 Terry Gillam science fiction film?",
+
+  "12 Monkeys",
+  "Time Bandits",
+  "Jupiter Ascending",
+  "The Zero Theorem",
+
+  "assets/images/twelvemonkeys.jpg"
+);
+var questionSeventeen = new QuestionObject(
+  "'Rick Deckard' is a character associated with which film that was directed by Ridley Scott?",
+
+  "Blade Runner",
+  "Prometheus",
+  "Legend",
+  "Alien",
+
+  "assets/images/bladerunner.jpg"
+);
+var questionEightteen = new QuestionObject(
+  "'Jane Fonda' travels 41st-century space with her blind guardian angel 'Pygar' in what 1968 science fiction film?",
+
+  "Barbarella",
+  "Thunderbird 6",
+  "Countdown",
+  "Voyage to the Planet of Prehistoric Women",
+
+  "assets/images/barbella.jpg"
+);
+var questionNineteen = new QuestionObject(
+  "Select the correct subtitle to the 1984 film 'Star Trek III'?",
+
+  "The Search for Spock",
+  "The Voyage Home",
+  "The Final Frontier",
+  "Wrath of Khan",
+
+  "assets/images/searchforspock.jpg"
+);
+var questionTwenty = new QuestionObject(
+  "What is the name of Malcolm McDowell's character in the 1971 film 'A Clockwork Orange'?",
+
+  "Alex DeLarge",
+  "Daniel DeLarge",
+  "Thomas DeLarge",
+  "Steven DeLarge",
+
+  "assets/images/alexdelargeclockworkorange.jpg"
+);
+var questionTwentyone = new QuestionObject(
+  "Who played the role of 'The Fly' (1986)?",
+
+  "Jeff Goldblum",
+  "Richard Attenborough",
+  "Michael Keaton",
+  "Sam Neill",
+
+  "assets/images/jeffgoldblumfly.jpg"
+);
+var questionTwentytwo = new QuestionObject(
+  "Released in 2006, which of the following films is an adaptation of a P. D. James' novel?",
+
+  "Children of Men",
+  "Altered",
+  "The Host",
+  "The Prestige",
+
+  "assets/images/childrenofmen.jpg"
+);
+var questionTwentythree = new QuestionObject(
+  "Who directed the 2009 epic science fiction film 'Avatar'?",
+
+  "James Cameron",
+  "Steven Spielberg",
+  "Ridley Scott",
+  "George Lucas",
+
+  "assets/images/avatar.jpg"
+);
+var questionTwentyfour = new QuestionObject(
+  "With which classic science fiction film would you associate the quote 'Game over, man. Game over!'?",
+
+  "Alien",
+  "Close Encounters of the Third Kind",
+  "The Thing",
+  "Predator",
+
+  "assets/images/aliengameover.jpg"
+);
+var questionTwentyfive = new QuestionObject(
+  "What year saw the release of the science fiction comedy film 'Men in Black'?",
+
+  "1997",
+  "1993",
+  "1999",
+  "1995",
+
+  "assets/images/meninblack.jpg"
+);
+var questionTwentysix = new QuestionObject(
+  "What is the name of the character that Tom Cruise plays in the 2014 film 'Edge of Tomorrow'?",
+
+  "Cage",
+  "Kimmel",
+  "Skinner",
+  "Farrell",
+
+  "assets/images/edgeoftmrw.jpg"
+);
+var questionTwentyseven = new QuestionObject(
+  "The Wachowski Brothers became well-known for which science fiction film series?",
+
+  "The Matrix",
+  "Men in Black",
+  "Jurassic Park",
+  "Predators",
+
+  "assetsimages\neomatrix.jpg"
+);
+var questionTwentyeight = new QuestionObject(
+  "'Kevin Spacey' and 'Sam Rockwell' co-star in which 2009 British science fiction film?",
+
+  "Moon",
+  "Gravity",
+  "Europa Report",
+  "Mission to Mars",
+
+  "assets/images/moon.jpg"
+);
+var questionTwentynine = new QuestionObject(
+  "Genetically perfect DNA is the subject of which science fiction film?",
+
+  "Gattaca",
+  "The Final Cut",
+  "Equilibrium",
+  "THX 1138",
+
+  "assets/images/gattaca.jpeg"
+);
+var questionThirty = new QuestionObject(
+  "'The Terminator' was the first film in which Arnold Schwarzenegger said what catchphrase?",
+
+  "Hasta La Vista, Baby",
+  "You're Terminated",
+  "Chill Out, Dickwad",
+  "I'll be Back",
+
+  "assets/images/hastalavistaTerminator.jpg"
 );
 
-//TO DO: Store all question here
-allQuestions = [templateQuestion, templateQuestion2];
+allQuestions = [
+  //TO DO: Store all question here
+  //templateQuestion
+
+  questionOne,
+  questionTwo,
+  questionThree,
+  questionFour,
+  questionFive,
+  questionSix,
+  questionSeven,
+  questionEight,
+  questionNine,
+  questionTen,
+  questionEleven,
+  questionTwelve,
+  questionThirteen,
+  questionFourteen,
+  questionFifteen,
+  questionSixteen,
+  questionSeventeen,
+  questionEightteen,
+  questionNineteen,
+  questionTwenty,
+  questionTwentyone,
+  questionTwentytwo,
+  questionTwentythree,
+  questionTwentyfour,
+  questionTwentyfive,
+  questionTwentysix,
+  questionTwentyseven,
+  questionTwentyeight,
+  questionTwentynine,
+  questionThirty
+];
 
 //#endregion
 
 //#region Methods
+
+function resetRoundTimer() {
+  clearInterval(roundTimeInteravl);
+  roundTime = 30;
+  timerElement.text("00:30");
+}
+
+function roundCounter() {
+  //Timer is zero or below
+  if (roundTime <= 0) {
+    //End Round(Run RoundCheck Method with unanswered parameter)
+    endRound("unanswered");
+  }
+  //Timer not Expired
+  else {
+    roundTime--;
+    var displayTime = timeConverter(roundTime);
+
+    //Display Time
+    timerElement.text(displayTime);
+  }
+}
+
+function timeConverter(t) {
+  var minutes = Math.floor(t / 60);
+  var seconds = t - minutes * 60;
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  if (minutes === 0) {
+    minutes = "00";
+  } else if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  return minutes + ":" + seconds;
+}
 
 var getElements = function() {
   //Panels
@@ -146,6 +511,7 @@ var getElements = function() {
 
   //Text Displays/Buttons
   gameStartButton = $("#gameStartButton");
+  gameOverButton = $("#gameOverButton");
   questionElement = $("#questionElement");
   answerAElement = $("#answerAElement");
   answerBElement = $("#answerBElement");
@@ -170,6 +536,9 @@ var score = function(_selectedQuestions) {
 var setGame = function() {
   selectedQuestions = [];
   currentRound = 0;
+  totalUnanswered = 0;
+  totalCorrect = 0;
+  totalIncorrect = 0;
   roundTime = 30;
   generateQuestions();
   console.log("Game (re)Set");
@@ -206,7 +575,7 @@ var newRound = function() {
   gameOverPanel.hide();
   questionPanel.show();
 
-  //TO DO: Start Round Timer
+  //Start Round Timer
   roundTimeInteravl = setInterval(roundCounter, 1000);
 };
 
@@ -237,12 +606,13 @@ var endRound = function(_result) {
   );
   solutionImage.attr("src", currentQuestions.image);
 
-  //TO DO: Show Results Panel
+  //Show Results Panel
   resultsPanel.show();
-  //TO DO: Start Queue Timer(newRound after timer expires, or game over)
+  //Increment Round
   currentRound++;
+  //Start Queue Timer(newRound after timer expires, or game over)
   if (currentRound < maxRounds) {
-    queueTimeInterval = setTimeout(newRound, 7000);
+    queueTimeInterval = setTimeout(newRound, 5000);
   } else {
     console.log("gameOver");
     queueTimeInterval = setTimeout(gameOver, 3000);
@@ -250,21 +620,20 @@ var endRound = function(_result) {
 };
 
 var gameOver = function() {
-  //
-  //TO DO: Hide Round Result Panel
+  // Hide Round Result Panel
   resultsPanel.hide();
   gameOverPanel.show();
 
-  //TO DO: Get Score
+  //Get Score
   var finalScore = (totalCorrect / maxRounds) * 100;
 
-  //TO DO: Load Game results to elements (incorrect, Correct, unanswered, and final percentage)
+  //Load Game results to elements (incorrect, Correct, unanswered, and final percentage)
   scoreElement.text(finalScore + "%");
   correctValueElement.text("Number of Correct: " + totalCorrect);
   incorrectValueElement.text("Number of Incorrect: " + totalIncorrect);
   unansweredValueElement.text("Number of Unanswered: " + totalUnanswered);
 
-  //TO DO: Reset Game
+  //Reset Game
   setGame();
 };
 
@@ -283,15 +652,8 @@ $(document).ready(function() {
   getElements();
   setGame();
 
-  ////Button Clicks
-  //TO DO: Add Start Game Button
+  //Button Clicks
   gameStartButton.on("click", newRound);
-
-  //TO DO: Add Answer Buttons
-  // answerAElement.on("click", endRound /* add Function Here */);
-  // answerBElement.on("click", endRound /* add Function Here */);
-  // answerCElement.on("click", endRound /* add Function Here */);
-  // answerDElement.on("click", endRound /* add Function Here */);
 
   answerAElement.on("click", function() {
     endRound(answerAElement.text());
@@ -306,63 +668,10 @@ $(document).ready(function() {
     endRound(answerDElement.text());
   });
 
-  $("#gameOverButton").on("click", newRound);
+  gameOverButton.on("click", newRound);
+  // $("#gameOverButton").on("click", newRound);
 });
 
 //#region
-
-/////
-
-//T
-function resetRoundTimer() {
-  clearInterval(roundTimeInteravl);
-  roundTime = 30;
-  timerElement.text("00:30");
-}
-
-function roundCounter() {
-  //Timer is zero or below
-  if (roundTime <= 0) {
-    //End Round(Run RoundCheck Method with unanswered parameter)
-    endRound("unanswered");
-  }
-  //Timer not Expired
-  else {
-    roundTime--;
-    var displayTime = timeConverter(roundTime);
-    console.log(" count function" + displayTime);
-
-    //Display Time
-    timerElement.text(displayTime);
-  }
-}
-
-//Queue Timer
-function QueueTimer() {
-  if (queueTime > 0) {
-    queueTime--;
-    console.log(queueTime);
-  } else {
-  }
-}
-
-//Time Converter
-function timeConverter(t) {
-  //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
-  var minutes = Math.floor(t / 60);
-  var seconds = t - minutes * 60;
-
-  if (seconds < 10) {
-    seconds = "0" + seconds;
-  }
-
-  if (minutes === 0) {
-    minutes = "00";
-  } else if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
-
-  return minutes + ":" + seconds;
-}
 
 //#endregion
